8626a685e40c74bb5f58b4c354835fdad2d67670
Dulybnis <ddn555@yandex.ru>
2024-05-06 04:41:43 +0500

    fix eslint error

--- src/application.js
+++ src/application.js
@@ -1,9 +1,8 @@
 import i18n from 'i18next';
 import * as yup from 'yup';
 import onChange from 'on-change';
-import resources from './locales/index.js';
 import axios from 'axios';
-
+import resources from './locales/index.js';
 
 export default () => {
   const stats = {
@@ -22,10 +21,10 @@ export default () => {
   };
 
   const i18ni = i18n;
-    i18ni.init({
-      lng: stats.language,
-      debug: false,
-      resources,
+  i18ni.init({
+    lng: stats.language,
+    debug: false,
+    resources,
   });
 
   const form = document.querySelector('.rss-form');
@@ -56,19 +55,19 @@ export default () => {
 
   const schema = yup.string().url().nullable();
 
-  const isNull = (url) => {
+  function isNull(url) {
     return new Promise((resolve, reject) => {
       if (url) {
         resolve(url);
       } else {
         reject(new Error('not to be null'));
       }
-    })
-  };
+    });
+  }
 
   const render = () => {
     pText.textContent = stats.status;
-    switch(stats.isTrue) {
+    switch (stats.isTrue) {
       case true:
         input.classList.remove('is-invalid');
         pText.classList.remove('text-danger');
@@ -84,51 +83,47 @@ export default () => {
 
   const feedsRender = () => {
     feeds.innerHTML = '';
-    if (stats.feedStats.feed.length < 1) {
-      return
-    } else {
+    if (stats.feedStats.feed.length > 0) {
       const h2El = document.createElement('div');
-      h2El.className = 'card border-0'
+      h2El.className = 'card border-0';
       h2El.innerHTML = '<div class="card-body"><h2 class="card-title h4">Фиды</h2></div><ul class="list-group border-0 rounded-0"></ul>';
       feeds.append(h2El);
       const ulEl = feeds.querySelector('ul');
-      stats.feedStats.feed.map((feedItem) => {
+      stats.feedStats.feed.forEach((feedItem) => {
         const liEl = document.createElement('li');
         liEl.className = 'list-group-item border-0 border-end-0';
-        liEl.innerHTML = `<h3 class="h6 m-0">${feedItem.title}</h3><p class="m-0 small text-black-50">${feedItem.description}</p>`
-        ulEl.append(liEl) 
-      })
+        liEl.innerHTML = `<h3 class="h6 m-0">${feedItem.title}</h3><p class="m-0 small text-black-50">${feedItem.description}</p>`;
+        ulEl.append(liEl);
+      });
     }
   };
 
   const postsRender = () => {
     posts.innerHTML = '';
-    if (stats.feedStats.post.length < 1) {
-      return
-    } else {
+    if (stats.feedStats.post.length > 0) {
       const h2El = document.createElement('div');
-      h2El.className = 'card border-0'
+      h2El.className = 'card border-0';
       h2El.innerHTML = '<div class="card-body"><h2 class="card-title h4">Посты</h2></div><ul class="list-group border-0 rounded-0"></ul>';
       posts.append(h2El);
       const ulEl = posts.querySelector('ul');
-      stats.feedStats.post.map((postItem) => {
+      stats.feedStats.post.forEach((postItem) => {
         const liEl = document.createElement('li');
         liEl.className = 'list-group-item d-flex justify-content-between align-items-start border-0 border-end-0';
-        liEl.innerHTML = `<a href="${postItem.link}" class="${postItem.fw}" data-id="${postItem.id}" target="_blank" rel="noopener noreferrer">${postItem.title}</a><button type="button" class="btn btn-outline-primary btn-sm" data-id="${postItem.id}" data-bs-toggle="modal" data-bs-target="#modal">Просмотр</button>`
+        liEl.innerHTML = `<a href="${postItem.link}" class="${postItem.fw}" data-id="${postItem.id}" target="_blank" rel="noopener noreferrer">${postItem.title}</a><button type="button" class="btn btn-outline-primary btn-sm" data-id="${postItem.id}" data-bs-toggle="modal" data-bs-target="#modal">Просмотр</button>`;
         const liButton = liEl.querySelector('.btn');
         liButton.addEventListener('click', (e) => {
           const idElement = e.target.previousSibling.getAttribute('data-id');
-          const idPost = stats.feedStats.post.find(element => element.id == idElement);
+          const idPost = stats.feedStats.post.find((element) => {return element.id === Number(idElement)});
           modalTitle.textContent = idPost.title;
           modalBody.textContent = idPost.description;
           idPost.fw = 'fw-normal';
           postsRender();
         });
         ulEl.append(liEl);
-      })
+      });
     }
   };
-  
+
   const progressRender = () => {
     if (stats.processed === 'in progress') {
       formButton.setAttribute('disabled', '');
@@ -151,20 +146,20 @@ export default () => {
 
   const watchedProgress = onChange(stats, () => {
     progressRender();
-  })
+  });
 
-  const parseRSS = (rss) => {
+  function parseRSS(rss) {
     return new Promise((resolve, reject) => {
       const parser = new DOMParser();
-      const parseRSS = parser.parseFromString(rss, 'application/xml');
-      const channel = parseRSS.querySelector('channel');
+      const parsingRSS = parser.parseFromString(rss, 'application/xml');
+      const channel = parsingRSS.querySelector('channel');
       if (channel) {
         resolve(channel);
       } else {
         reject(new Error('channel is null'));
       }
-    })
-  };
+    });
+  }
 
   const postsAdd = (channel) => {
     const ownTitle = stats.feedStats.post.map((post) => post.title);
@@ -182,7 +177,7 @@ export default () => {
           fw: 'fw-bold',
         });
       }
-    })
+    });
     if (newPostStats.length > 0) {
       watchedPostState.unshift(...newPostStats);
     }
@@ -200,65 +195,65 @@ export default () => {
     watchedFeedState.push(newFeedStats);
   };
 
-  const getRSS = (url) => {
+  function getRSS(url) {
     return new Promise((resolve, reject) => {
       axios.get(`https://allorigins.hexlet.app/get?disableCache=true&url=${encodeURIComponent(url)}`)
         .then((rss) => resolve(rss))
-        .catch(() => reject(new Error('network error')))
-    })
-  };
-
-  const addRSS = (channel, url) => {
-    return new Promise((resolve, reject) => {
-        if (!stats.urls.includes(url)) {
-          stats.status = i18ni.t('text.rssAdded');
-          stats.isTrue = true;
-          input.value = '';
-          input.focus();
-          watchedSubmit.urls.push(url);
-          feedAdd(channel);
-          if (stats.refresh === 'off') {
-            stats.refresh = 'on';
-            refresh();
-          }
-          resolve();
-        } else {
-          reject(new Error('RSS already exists'));
-        }
-    })
-  };
-
-  const addUrl = (url) => {
-    watchedProgress.processed = 'in progress';
-    stats.isTrue = true;
-    watchedSubmit.status = i18ni.t(`text.writing`);
-    isNull(url)
-      .then((url) => schema.validate(url))
-      .then((data) => getRSS(data))
-      .then((rss) => parseRSS(rss.data.contents))
-      .then((channel) => addRSS(channel, url))
-      .catch((e) => {
-        stats.isTrue = false;
-        watchedSubmit.status = i18ni.t(`text.${e.message}`);
-      })
-      .then(() => watchedProgress.processed = 'finish')
-  };
+        .catch(() => reject(new Error('network error')));
+    });
+  }
 
   const refresh = () => {
     if (stats.urls.length > 0) {
       stats.urls.forEach((url) => {
         getRSS(url)
-        .then((rss) => parseRSS(rss.data.contents))
-        .then((channel) => postsAdd(channel))
-      })
+          .then((rss) => parseRSS(rss.data.contents))
+          .then((channel) => postsAdd(channel));
+      });
     }
     setTimeout(() => {
       refresh();
     }, 5000);
   };
 
+  function addRSS(channel, url) {
+    return new Promise((resolve, reject) => {
+      if (!stats.urls.includes(url)) {
+        stats.status = i18ni.t('text.rssAdded');
+        stats.isTrue = true;
+        input.value = '';
+        input.focus();
+        watchedSubmit.urls.push(url);
+        feedAdd(channel);
+        if (stats.refresh === 'off') {
+          stats.refresh = 'on';
+          refresh();
+        }
+        resolve();
+      } else {
+        reject(new Error('RSS already exists'));
+      }
+    });
+  }
+
+  const addUrl = (url) => {
+    watchedProgress.processed = 'in progress';
+    stats.isTrue = true;
+    watchedSubmit.status = i18ni.t('text.viewing');
+    isNull(url)
+      .then((notNullUrl) => schema.validate(notNullUrl))
+      .then((validateUrl) => getRSS(validateUrl))
+      .then((rss) => parseRSS(rss.data.contents))
+      .then((channel) => addRSS(channel, url))
+      .catch((e) => {
+        stats.isTrue = false;
+        watchedSubmit.status = i18ni.t(`text.${e.message}`);
+      })
+      .then(() => {watchedProgress.processed = 'finish'});
+  };
+
   form.addEventListener('submit', (e) => {
     e.preventDefault();
-    addUrl(input.value)
-  })
+    addUrl(input.value);
+  });
 };
